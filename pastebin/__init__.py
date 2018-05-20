cd922a5d18848133c57872e39f3999dd98c3e828
Matt <mathewlogan90@gmail.com>
2018-05-20 11:12:04 -0400

    Added PastebinUserParser and PastebinUser classes

--- pastebin/__init__.py
+++ pastebin/__init__.py
@@ -7,6 +7,7 @@ Pastebin API wrapper using urllib
 '''
 
 
+from collections import OrderedDict
 from html.parser import HTMLParser
 from http.client import HTTPException
 from urllib.request import urlopen
@@ -26,6 +27,8 @@ class Pastebin:
         create_paste
         create_logged_in_paste
         list_pastes
+        list_trending_pastes
+        delete_paste
     '''
 
     def __init__(self, api_key):
@@ -35,9 +38,9 @@ class Pastebin:
     def __repr__(self):
         return 'Pastebin(%s)'.format(self.api_key)
 
-    def _get(self, url, data=None, **kwargs):
+    def _request(self, url, data=None, **kwargs):
         '''
-        A custom GET method for the Pastebin API wrapper
+        A custom request method for the Pastebin API wrapper.
 
         args:
             url (str)
@@ -46,23 +49,23 @@ class Pastebin:
 
         returns:
             http.client.HTTPResponse object
+
+        raises:
+            http.client.HTTPException if the response code is not 200
         '''
         response = urlopen(url, data=data)
         if response.status == 200:
             return response
         else:
-            raise HTTPError
+            raise HTTPException
 
     def login(self, user_name, user_password):
         '''
-        Use the Pastebin members system to login and create
-        pastes as a logged in user.
+        Use the Pastebin members system to login
 
         args:
             user_name (bytes): the user name to login to
             user_password (bytes): the user password to use to login
-
-        kwargs:
 
         returns:
            Pastebin API user key (bytes)
@@ -74,8 +77,8 @@ class Pastebin:
         }
         data = urlencode(data)
         data = bytes(data, encoding='utf-8')
-        response = self._get('https://pastebin.com/api/api_login.php',
-                             data=data)
+        response = self._request('https://pastebin.com/api/api_login.php',
+                                 data=data)
         self.user_key = response.read()
         return self.user_key
 
@@ -89,7 +92,8 @@ class Pastebin:
             paste_code (bytes): the code to paste to Pastebin
 
         kwargs:
-            user_key (bytes): part of the login system
+            user_key (bytes): call the login() method to receive a user
+                              key
             paste_name (bytes): this will be the name of the paste
             paste_format (bytes): the syntax highlighting value
             paste_private (int): default is 0; public=0, unlisted=1,
@@ -125,15 +129,17 @@ class Pastebin:
             data['api_paste_expire_date'] = paste_expire_date
         data = urlencode(data)
         data = bytes(data, encoding='utf-8')
-        response = self._get('https://pastebin.com/api/api_post.php',
-                             data=data)
+        response = self._request('https://pastebin.com/api/api_post.php',
+                                 data=data)
         return response
 
-    def create_logged_in_paste(self, paste_code, user_key=None,
-                               paste_name=None,paste_format=None,
-                               paste_private=0,paste_expire_date=None):
+    def create_logged_in_paste(self, paste_code, paste_name=None,
+                               paste_format=None, paste_private=0,
+                               paste_expire_date=None):
         '''
         Create a new logged in paste. Must call Pastebin.login first.
+        Calls create_paste() method with user_key keyword set to
+        self.user_key
 
         args:
             paste_code (bytes): the code to paste to Pastebin
@@ -157,27 +163,19 @@ class Pastebin:
 
         returns:
             http.client.HTTPResponse object
+
+        raises:
+            AttributeError if self.user_key is not set
         '''
         if not self.user_key:
             raise AttributeError('''user_key is not set. Login first to
                                  create a logged in paste.''')
-        data = {
-            'api_dev_key' : self.api_key,
-            'api_option' : b'paste',
-            'api_paste_code' : paste_code,
-            'api_user_key' : self.user_key
-        }
-        if paste_name:
-            data['api_paste_name'] = paste_name
-        if paste_format:
-            data['api_paste_format'] = paste_format
-        data['api_paste_private'] = paste_private
-        if paste_expire_date:
-            data['api_paste_expire_date'] = paste_expire_date
-        data = urlencode(data)
-        data = bytes(data, encoding='utf-8')
-        response = self._get('https://pastebin.com/api/api_post.php',
-                             data=data)
+        response = self.create_paste(paste_code, user_key=self.user_key,
+                                     paste_name=paste_name,
+                                     paste_format=paste_format,
+                                     paste_private=paste_private,
+                                     paste_expire_date=paste_expire_date
+                                    )
         return response
 
     def list_pastes(self, results_limit=5, parse=False):
@@ -195,8 +193,9 @@ class Pastebin:
             http.client.HTTPResponse object
         '''
         if not self.user_key:
-            raise AttributeError('''user_key is not set. Login first to
-                                 create a logged in paste.''')
+            raise AttributeError('''user_key is not set.
+                                 Login first to create a logged
+                                 in paste.''')
         data = {
             'api_dev_key' : self.api_key,
             'api_user_key' : self.user_key,
@@ -205,13 +204,100 @@ class Pastebin:
         }
         data = urlencode(data)
         data = bytes(data, encoding='utf-8')
-        response = self._get('https://pastebin.com/api/api_post.php',
-                             data=data)
+        response = self._request('https://pastebin.com/api/api_post.php',
+                                 data=data)
         if parse:
             response = response.read().decode(encoding='utf-8')
             parser = PastebinPasteListParser()
             pastes = parser.get_pastes(response)
             return pastes
+        return response
+
+    def list_trending_pastes(self, parse=False):
+        '''
+        List the 18 currently trending pastes.
+
+        kwargs:
+            parse (bool): If this is true, a list of PastebinPastes
+                          objects will be returned instead of the
+                          usual HTTPResponse
+
+        returns:
+            http.client.HTTPResponse object
+        '''
+        data = {
+            'api_dev_key' : self.api_key,
+            'api_option' : b'trends'
+        }
+        data = urlencode(data)
+        data = bytes(data, encoding='utf-8')
+        response = self._request('https://pastebin.com/api/api_post.php',
+                                 data=data)
+        if parse:
+            response = response.read().decode(encoding='utf-8')
+            parser = PastebinPasteListParser()
+            pastes = parser.get_pastes(response)
+            return pastes
+        return response
+
+    def delete_paste(self):
+        '''
+        Delete pastes created by a user.
+
+        args:
+            paste_key
+
+        returns:
+            http.client.HTTPResponse object
+        '''
+        if not self.user_key:
+            raise AttributeError('''user_key is not set.
+                                 Login first to delete a paste.''')
+        data = {
+            'api_dev_key' : self.api_key,
+            'api_user_key' : self.user_key,
+            'api_paste_key' : paste_key,
+            'api_option' : b'delete'
+        }
+        data = urlencode(data)
+        data = bytes(data, encoding='utf-8')
+        response = self._request('https://pastebin.com/api/api_post.php',
+                                 data=data)
+        return response
+
+    def get_user_information(self, parse=False):
+        '''
+        Obtain a user's personal information and settings.
+        Must be logged in first.
+
+        kwargs:
+            parse (bool): If this is true, a list of PastebinPastes
+                          objects will be returned instead of the
+                          usual HTTPResponse
+
+        returns:
+            http.client.HTTPResponse object
+
+        raises:
+            AttributeError if self.user_key is not set
+        '''
+        if not self.user_key:
+            raise AttributeError('''user_key is not set.
+                                 Login first to delete a paste.''')
+        data = {
+            'api_dev_key' : self.api_key,
+            'api_user_key' : self.user_key,
+            'api_option' : b'userdetails'
+        }
+        data = urlencode(data)
+        data = bytes(data, encoding='utf-8')
+        response = self._request('https://pastebin.com/api/api_post.php',
+                                 data=data)
+        if parse:
+            response = response.read().decode(encoding='utf-8')
+            parser = PastebinUserParser()
+            users = parser.get_user_information(response)
+            return users
         return response
 
 
@@ -263,6 +349,32 @@ class PastebinPasteListParser(HTMLParser):
         return pastes
 
 
+class PastebinUserParser(HTMLParser):
+    '''
+    A custom parser for the get_ user_information method of the
+    Pastebin API.
+    Mostly for internal use on the Pastebin.get_ user_information
+    method when the parse flag is true.
+    '''
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self._user_dict = OrderedDict()
+
+    def get_user_information(self, data):
+        self.feed(data)
+        return self._get_user_information()
+
+    def handle_data(self, data):
+        tag = self.get_starttag_text()[1:-1]
+        data = data
+        if data != '\r\n':
+            self._user_dict[tag] = data
+
+    def _get_user_information(self):
+        user = PastebinUser(**self._user_dict)
+        return user
+
+
 class PastebinPaste:
     '''
     This class encapsulates a Pastebin paste
@@ -299,4 +411,41 @@ class PastebinPaste:
                               for key in self.__dict__.keys()])
 
 
-__all__ = ['Pastebin', 'PastebinPasteListParser', 'PastebinPaste']
+class PastebinUser:
+    '''
+    This class encapsulates a Pastebin user.
+
+    attributes:
+        user_name (bytes)
+        user_format_short (bytes)
+        user_expiration (bytes)
+        user_avatar_url (bytes)
+        user_private (int)
+        user_website (bytes)
+        user_email (bytes)
+        user_location (bytes)
+        user_account_type (int)
+    '''
+    def __init__(self, user_name=None, user_format_short=None,
+                 user_expiration=None,
+                 user_avatar_url=None, user_private=None,
+                 user_website=None, user_email=None,
+                 user_location=None, user_account_type=None):
+        self.user_name = user_name
+        self.user_format_short = user_format_short
+        self.user_expiration = user_expiration
+        self.user_avatar_url = user_avatar_url
+        self.user_private = user_private
+        self.user_website = user_website
+        self.user_email = user_email
+        self.user_location = user_location
+        self.user_account_type = user_account_type
+
+    def __repr__(self):
+        _repr = '''PastebinUser(user_name={}, user_format_short={}, user_expiration={}, user_avatar_url={}, user_private={}, user_website={}, user_email={}, user_location={}, user_account_type={})'''
+        return _repr.format(*[getattr(self, str(key))
+                              for key in self.__dict__.keys()])
+
+
+__all__ = ['Pastebin', 'PastebinPasteListParser', 'PastebinPaste',
+           'PastebinUserParser', 'PastebinUser']
