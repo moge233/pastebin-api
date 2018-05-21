ddbd1fbb65683474a7f3d3d7d6c30f8f532a1c1c
Matt <mathewlogan90@gmail.com>
2018-05-20 20:16:21 -0400

    Added tests

---
+++ tests/test_pastebin.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+'''
+test_pastebin.py
+
+Provides automated tests for the Pastebin API wrapper.
+'''
+
+
+from unittest import TestCase, TestSuite
+
+
+class RequestTestCase(TestCase):
+    pass
+
+class LoginTestCase(TestCase):
+    pass
+
+class CreatePasteTestCase(TestCase):
+    pass
+
+class CreateLoggedInPasteTestCase(TestCase):
+    pass
+
+class ListPastesTestCase(TestCase):
+    pass
+
+class ListTrendingPastesTestCase(TestCase):
+    pass
+
+class DeletePasteTestCase(TestCase):
+    pass
+
+class GetUserInformationTestCase(TestCase):
+    pass
+
+
+tests = [
+    RequestTestCase, LoginTestCase, CreatePasteTestCase,
+    CreateLoggedInPasteTestCase, ListPastesTestCase,
+    ListTrendingPastesTestCase, DeletePasteTestCase,
+    GetUserInformationTestCase
+]
+
+
